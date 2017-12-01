66ea93090ce7bd68f2721074f45c5ebd0f64e77c
Yves Goeleven <yves@goeleven.com>
2017-12-01 19:01:18 +0100

    form design

--- _site/js/promotion.js
+++ _site/js/promotion.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         dataType: 'json', // use json only, not jsonp
         crossDomain: true, // tell browser to allow cross domain.
         success: function(promotion){ 
-            promotionholder.text(promotion.name);
+            //promotionholder.text(promotion.name);
         }
       });
 });
