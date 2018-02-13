c73356c5484b4938134700ea9da66c3aab52653e
Yves Goeleven <yves@goeleven.com>
2018-02-13 23:17:46 +0100

    do not render archived shifts & tasks

--- _site/js/workplanning.js
+++ _site/js/workplanning.js
@@ -1,37 +1,39 @@
 function renderHeader(planning){
 	var tr = $("#workplanning-table tr:first");
 	planning.shifts.forEach(function(shift) {
-		var start = new Date(Date.parse(shift.workStartsOn));
-		var end = new Date(Date.parse(shift.workEndsOn));
-		var div = $.template("#shift-template",
-		{
-			title: start.toLocaleDateString('nl-BE', { year: 'numeric', month: 'numeric', day: 'numeric' }) + " [" + start.toLocaleTimeString('nl-BE', { hour: 'numeric', minute: 'numeric' }) + "-" + end.toLocaleTimeString('nl-BE', { hour: 'numeric', minute: 'numeric' }) + "]"
-		});
-		tr.append($("<th>").addClass("responsive-table-cell").append(div));
-		
+		if(!shift.archived){
+			var start = new Date(Date.parse(shift.workStartsOn));
+			var end = new Date(Date.parse(shift.workEndsOn));
+			var div = $.template("#shift-template",
+			{
+				title: start.toLocaleDateString('nl-BE', { year: 'numeric', month: 'numeric', day: 'numeric' }) + " [" + start.toLocaleTimeString('nl-BE', { hour: 'numeric', minute: 'numeric' }) + "-" + end.toLocaleTimeString('nl-BE', { hour: 'numeric', minute: 'numeric' }) + "]"
+			});
+			tr.append($("<th>").addClass("responsive-table-cell").append(div));		
+		}		
 	});
 }
 
 function renderRows(planning){
 	planning.tasks.forEach(function(task) {
-		var table = $("#workplanning-table");
-		var div = $.template("#task-template",
-		{
-			description: task.description
-		});
-		var tr = $("<tr>");
-		table.append(tr.append($("<td>").addClass("responsive-table-cell").append(div)));
-		for (i = 0; i < planning.shifts.length; i++) {
-			var shift = planning.shifts[i];
-			var slots = planning.slots.filter(function(e){ return e.workTaskId === task.id && e.workShiftId == shift.id; });
-			var slot = slots.length > 0 ? slots[0] : null;
-			var slotid = task.id + "-" + shift.id;
-			
-			var content = renderSlotContent(task.id, shift.id, slot);					
-			
-			tr.append($("<td>").attr('id', slotid).addClass("responsive-table-cell").append(content));
+		if(!task.archived){
+			var table = $("#workplanning-table");
+			var div = $.template("#task-template",
+			{
+				description: task.description
+			});
+			var tr = $("<tr>");
+			table.append(tr.append($("<td>").addClass("responsive-table-cell").append(div)));
+			for (i = 0; i < planning.shifts.length; i++) {
+				var shift = planning.shifts[i];
+				var slots = planning.slots.filter(function(e){ return e.workTaskId === task.id && e.workShiftId == shift.id; });
+				var slot = slots.length > 0 ? slots[0] : null;
+				var slotid = task.id + "-" + shift.id;
+				
+				var content = renderSlotContent(task.id, shift.id, slot);					
+				
+				tr.append($("<td>").attr('id', slotid).addClass("responsive-table-cell").append(content));
+			}
 		}
-		
 	});
 }
 function renderSlotContent(taskid, shiftid, slot){
