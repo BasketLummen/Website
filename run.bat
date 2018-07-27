b27a8d77475573357b71d90ff202f62a4cbca94c
Yves Goeleven <yves@goeleven.com>
2018-07-27 15:40:30 +0200

    run on port 5001

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll --theme BasketLummen
+dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll --theme BasketLummen --port 5001
