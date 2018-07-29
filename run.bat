f8060b69324fa8fd275f2f8f96d6ec7d68d96598
Yves Goeleven <yves@goeleven.com>
2018-07-29 18:24:34 +0200

    publish support

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll --theme BasketLummen --port 5001
+dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll serve --theme BasketLummen --port 5001
