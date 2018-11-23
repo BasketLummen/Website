82b36fc150e08d092c98c68b744ff7ce4f51830f
Yves Goeleven <yves@goeleven.com>
2018-11-23 12:49:48 +0100

    polyfill for IE

--- _apps/Club/js/init.js
+++ _apps/Club/js/init.js
@@ -60,3 +60,15 @@ if (!String.prototype.format) {
     });
   };
 }
+
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, "includes", {
+    enumerable: false,
+    value: function(obj) {
+        var newArr = this.filter(function(el) {
+          return el == obj;
+        });
+        return newArr.length > 0;
+      }
+  });
+}
