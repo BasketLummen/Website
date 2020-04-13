c0907b2083b2cf76bd5ea8ef173d72096355e2d8
Sean Feldman <feldman.sean@gmail.com>
2020-04-13 13:24:34 -0600

    Buttons working

--- _apps/ClubManagement.Donation/js/donations.component.donate.js
+++ _apps/ClubManagement.Donation/js/donations.component.donate.js
@@ -14,7 +14,17 @@ class Donate extends HTMLElement {
         var donate5 = this.querySelector("#donate5");
         var donate10 = this.querySelector("#donate10");
         var donate20 = this.querySelector("#donate20");
-        var donateOther = this.querySelector("#donateOther");
+        var donation = this.querySelector("#donateOther");
+
+        donate5.addEventListener('click', async (event) =>{
+            donation.value = 5;
+        });
+        donate10.addEventListener('click', async (event) =>{
+            donation.value = 10;
+        });
+        donate20.addEventListener('click', async (event) =>{
+            donation.value = 20;
+        });
 
         form.addEventListener('submit', async (event) => {
             event.preventDefault();
