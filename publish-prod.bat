bdeb5e8509e6c0411769de125a3c99452c6dcd9d
Sean Feldman <feldman.sean@gmail.com>
2020-05-02 13:47:15 -0600

    Split prod and test Application Insights

--- publish-prod.bat
+++ publish-prod.bat
@@ -1,1 +1,1 @@
-dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll publish --theme BasketLummen --url "https://www.basketlummen.be/" --connectionStringName dish-publish-lummen-prod 
+dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll publish --theme BasketLummen --url "https://www.basketlummen.be/" --connectionStringName dish-publish-lummen-prod  --env "prod"
