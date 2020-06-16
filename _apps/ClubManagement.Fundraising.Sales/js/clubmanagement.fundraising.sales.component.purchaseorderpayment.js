33cac7d454aeb1efd7ff358e8b6d6fca99280bcc
Sean Feldman <feldman.sean@gmail.com>
2020-06-16 13:21:29 -0600

    Initial rendering of the payment method options

--- _apps/ClubManagement.Fundraising.Sales/js/clubmanagement.fundraising.sales.component.purchaseorderpayment.js
+++ _apps/ClubManagement.Fundraising.Sales/js/clubmanagement.fundraising.sales.component.purchaseorderpayment.js
@@ -6,7 +6,10 @@ import { guid } from "/js/clubmanagement.guid.js"
 class PurchaseOrderPayment extends HTMLElement {
 
     constructor(){
-        super();       
+        super();
+        
+        this.template = document.getElementById("clubmgmt-purchase-order-payment-template");
+        this.paymentMethodTemplate = document.getElementById("clubmgmt-purchase-order-payment-method-template");
     }
 
     static get observedAttributes() {
@@ -62,7 +65,26 @@ class PurchaseOrderPayment extends HTMLElement {
   	} 
 
     async connectedCallback() {
+        const content = this.template.content.cloneNode(true);
+        const form = content.querySelector("#orderPayment");
+        const table = content.querySelector("#paymentMethodOptions");
+                
+        const sale = await this.loadSale();
+        
+        if (sale.allowedPaymentMethodTypes.length === 0) {
+            sale.allowedPaymentMethodTypes.push({
+                id: "cash", name: "Cash"
+            });    
+        }
 
+        for (let paymentMethodType of sale.allowedPaymentMethodTypes) {
+            const rowContent = this.paymentMethodTemplate.content.cloneNode(true)
+            this.renderPaymentMethodOption(rowContent, paymentMethodType.id, paymentMethodType.name);
+            table.append(rowContent);
+        }
+        
+        this.append(content);
+        
         // const title = document.createElement("div");
         // title.innerText = `TODO: Set up payment for order ${this.orderId} amount: ${this.currency}${this.total}`;
         // this.append(title);
@@ -85,8 +107,8 @@ class PurchaseOrderPayment extends HTMLElement {
     }
 
     async loadSale(){
-        var uri = `${salesConfig.salesService}/api/sales/${club.organizationId}/${this.saleId}`;
-        var request = await fetch(uri, {
+        const uri = `${salesConfig.salesService}/api/sales/${club.organizationId}/${this.saleId}`;
+        const request = await fetch(uri, {
             method: "GET",
             mode: 'cors',
             headers: {
@@ -94,6 +116,14 @@ class PurchaseOrderPayment extends HTMLElement {
             }
         });
         return await request.json();
+    }
+    
+    renderPaymentMethodOption(rowElement, paymentMethodId, paymentMethodName) {
+        const radioButton = rowElement.querySelector("input[type='radio']");
+        radioButton.setAttribute("value", paymentMethodId);
+                
+        const title = rowElement.querySelector("span");
+        title.innerText = paymentMethodName;
     }
 }
 
