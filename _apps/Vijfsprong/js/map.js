f72a4baca1b62a298e27cec127fffc8667afbd79
Yves Goeleven <yves@goeleven.com>
2022-05-29 22:44:13 +0200

    add all training facilities for next season

--- _apps/Vijfsprong/js/map.js
+++ _apps/Vijfsprong/js/map.js
@@ -1,6 +1,42 @@
-function initMap() {
+function initVijfsprong() {
+    var el = document.getElementById('vijfsprong');
+    if(!el) return;
+
     var loc = {lat: 50.991189, lng: 5.18965};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(el, {
+      zoom: 15,
+      center: loc
+    });
+    /*var marker = new google.maps.Marker({
+      position: loc,
+      map: map
+    });*/
+
+    
+  var infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+
+  service.getDetails({ placeId: 'ChIJZRgmzYI7wUcRjEHmCqFIOFA' }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+            place.formatted_address + '</div>');
+        infowindow.open(map, this);
+      });
+      }
+  });
+}
+
+function initOHVM() {
+  var el = document.getElementById('ohvm');
+  if(!el) return;
+
+  var loc = {lat: 50.98634960469504, lng: 5.194314250017507}; 
+    var map = new google.maps.Map(el, {
       zoom: 15,
       center: loc
     });
@@ -9,19 +45,56 @@ function initMap() {
       map: map
     });*/
   var infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
+  var service = new google.maps.places.PlacesService(map);
 
-    service.getDetails({ placeId: 'ChIJZRgmzYI7wUcRjEHmCqFIOFA' }, function(place, status) {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+  service.getDetails({ placeId: 'ChIJB9uhJ3k7wUcRLGCy9wDZbzA' }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
             place.formatted_address + '</div>');
-          infowindow.open(map, this);
-        });
+        infowindow.open(map, this);
+      });
       }
-    });
-  }
+  });
+}
+
+function initKambergen() {
+  var el = document.getElementById('kambergen');
+  if(!el) return;
+
+  var loc = {lat: 50.973477484731156, lng: 5.123007369493038};
+  var map = new google.maps.Map(el, {
+    zoom: 15,
+    center: loc
+  });
+  /*var marker = new google.maps.Marker({
+    position: loc,
+    map: map
+  });*/
+  var infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+
+  service.getDetails({ placeId: 'ChIJkZ1Jd7A-wUcR4Bv5dTUY-Vk' }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+          place.formatted_address + '</div>');
+        infowindow.open(map, this);
+      });
+    }
+  });
+}
+
+function initMaps() {
+  initVijfsprong();
+  initOHVM();
+  initKambergen();
+}
