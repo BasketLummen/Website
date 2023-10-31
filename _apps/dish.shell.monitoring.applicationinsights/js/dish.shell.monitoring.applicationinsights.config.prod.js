937e88459629e80e09729f0e4f174d5307988798
Yves Goeleven <yves@goeleven.com>
2023-10-31 23:16:19 +0100

    fix correlation

--- _apps/dish.shell.monitoring.applicationinsights/js/dish.shell.monitoring.applicationinsights.config.prod.js
+++ _apps/dish.shell.monitoring.applicationinsights/js/dish.shell.monitoring.applicationinsights.config.prod.js
@@ -1,7 +1,8 @@
 const appinsightsConfig = {
     instrumentationKey: "fd161f76-2256-4df2-a707-36ae6140ea51",
 	role: "basketlummen.be",
-	roleInstance: "www.basketlummen.be"
+	roleInstance: "www.basketlummen.be",
+	correlationHeaderDomains: ['*.clubmanagement.io', '*.azurewebsites.net']
 };
 
 export { appinsightsConfig }
