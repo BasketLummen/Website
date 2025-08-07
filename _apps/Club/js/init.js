05d02b65ba92c940d84abdf7aaa3fb4211720419
yvesgoeleven <yves@goeleven.com>
2025-08-07 22:35:23 +0200

    fix team name on home page

--- _apps/Club/js/init.js
+++ _apps/Club/js/init.js
@@ -2,7 +2,7 @@ var orgId = "5159e64f-4d2e-42c4-968d-6ff38338129b";
 var vblOrgId = "BVBL1176";
 
 //var partnerTeamNames = ["Hasselt BT ", "Basket Zonhoven ", "B.C. Gems Diepenbeek "];
-var partnerTeamNames = [];
+var partnerTeamNames = ["Basket Lummen"];
 //var partnerTeamIds = ["BVBL1087M19%20%201", "BVBL1087M16%20%201", "BVBL1087M16%20%202", "BVBL1087M14%20%201", "BVBL1087M14%20%202", "BVBL1087G14%20%201"];
 var partnerTeamIds = [];
 
